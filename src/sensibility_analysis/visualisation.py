0c9cae32e64fda0a5a02dcaf743728226e9d26cd
Octave Jagora <octave.jagora@telecom-paris.fr>
2023-07-29 15:04:20 +0200

    Adding visualisation of all weight envelopes

--- src/sensibility_analysis/visualisation.py
+++ src/sensibility_analysis/visualisation.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from deltas import genDeltas,getDelta
+from matplotlib.collections import LineCollection
+from confidence import g
 
-df = pd.read_csv('C:\\Users\\octav\\Desktop\\stage\\data\\octave.csv')
+path = '././ressources/mia.csv'
+df = pd.read_csv(path)
 X = np.array([df['avrege_gnps'],df['score_gnps'],
      df['avrege_sirius'],df['score_sirius'],
      df['avrege_isdb'],df['score_isdb']])
@@ -54,4 +57,114 @@ def delta_heatmap(X,n=100,p=11):
     plt.show()
     return
 
-delta_heatmap(X)
+def plotWt(X,n=1000,w=10) :
+    W = np.linspace(0,1,n)
+    G = []
+    col = []
+    for i in range(n):
+        G.append(g(0.7,0.2,0.1,W[i],X))
+        col.append('g'+str(i))
+    inds = G[int(n/2)].argsort()
+    for i in range(n) :
+        #inds = G[i].argsort()
+        G[i] = G[i][inds[::-1]]
+
+    G = np.transpose(G)
+    df = pd.DataFrame(G, columns = col)
+    for lab in col :
+        df[lab] = df[lab].rolling(w).mean()
+
+    df.dropna(inplace=True)
+
+    GS = df.to_numpy()
+
+    GS = np.transpose(GS)
+    
+    x = np.linspace(0,1,len(GS[-1]))
+    xs = np.tile(x,(n,1))
+
+    fig,ax = plt.subplots()
+    #np.interp(y, (y.min(), y.max()), (0,1))
+
+    segments = [np.column_stack([x, y]) for x, y in zip(xs, GS)]
+    lc = LineCollection(segments,cmap = "coolwarm")
+    lc.set_array(np.asarray(W))
+    ax.add_collection(lc)
+    ax.autoscale()
+
+    axcb = fig.colorbar(lc)
+    plt.suptitle("g rolling mean distribution in function of the value of wt")
+    plt.title("rolling frame of size 10")
+    axcb.set_label("wt")
+    plt.xticks([])
+    plt.show()
+    return
+
+def plotW(ax,X,W,p,n) :
+    if p == 1 :
+        ind = 2
+        W1 = W*np.ones(n)
+        W2 = np.linspace(0,1-W,n)
+        W3 = np.linspace(1-W,0,n)
+    elif p == 2 :
+        ind = 3
+        W2 = W*np.ones(n)
+        W3 = np.linspace(0,1-W,n)
+        W1 = np.linspace(1-W,0,n)
+    else :
+        ind = 1
+        W3 = W*np.ones(n)
+        W1 = np.linspace(0,1-W,n)
+        W2 = np.linspace(1-W,0,n)
+        
+    G = []
+    col = []
+    for i in range(n):
+        G.append(g(W1[i],W2[i],W3[i],0.5,X))
+        col.append('g'+str(i))
+    inds = G[int(n/2)].argsort()
+    for i in range(n) :
+        G[i] = G[i][inds[::-1]]
+
+    G = np.transpose(G)
+    df = pd.DataFrame(G, columns = col)
+    for lab in col :
+        df[lab] = df[lab].rolling(100).mean()
+
+    df.dropna(inplace=True)
+    GS = df.to_numpy()
+    GS = np.transpose(GS)
+    
+    x = np.linspace(0,1,len(GS[-1]))
+    xs = np.tile(x,(n,1))
+    #fig,ax = plt.subplots()
+    segments = [np.column_stack([x, np.interp(y, (y.min(), y.max()), (0,1))]) for x, y in zip(xs, GS)]
+    lc = LineCollection(segments,cmap = "coolwarm")
+    
+    if p == 1 :
+        lc.set_array(np.asarray(W2))
+    elif p == 2 :
+        lc.set_array(np.asarray(W3))
+    else :
+        lc.set_array(np.asarray(W1))
+        
+    ax.add_collection(lc)
+    ax.autoscale()
+    #axcb = fig.colorbar(lc)
+    #axcb.set_label("w"+str(ind))
+
+def visualize_weights_enveloppes(W = [0.1,0.3,0.5,0.7,0.9],n=1000):
+    fig, axs = plt.subplots(3,5)
+    for j in range(3) :
+        for i in range(len(W)) :
+            plotW(axs[j,i],X,W[i],j+1,n)
+            axs[j,i].set_xticks([])
+            if i != 0 :
+                axs[j,i].set_yticks([])
+            if i == 0 :
+                axs[j,i].set_ylabel("Wi = W"+str(j+1))
+            if j == 0 :
+                axs[j,i].set_title("Wi = " + str(W[i]))
+    plt.show()
+
+visualize_weights_enveloppes()
