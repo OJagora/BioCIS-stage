e9f18f5897199924a4fdd76af77bb88d34b04948
Octave Jagora <octave.jagora@telecom-paris.fr>
2023-07-29 14:22:12 +0200

    adding defintion of g

---
+++ src/sensibility_analysis/confidence.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def g(w1,w2,w3,wt,X):
+    X = np.nan_to_num(X)
+    a1 = X[0,:]
+    s1 = X[1,:]
+    a2 = X[2,:]
+    s2 = X[3,:]
+    a3 = X[4,:]
+    s3 = X[5,:]
+    res1 = (1-wt)*s1 + wt*a1
+    res2 = (1-wt)*s2 + wt*a2
+    res3 = (1-wt)*s3 + wt*a3
+    R = np.array([res1,res2,res3])
+    R = np.transpose(R)
+    R = np.sort(R)
+    res = w1*R[:,2] + w2*R[:,1] + w3*R[:,0]
+    return res
