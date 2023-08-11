2b8292f0c1c68be804b812d287ea1ab3934d3948
Octave Jagora <octave.jagora@telecom-paris.fr>
2023-08-11 19:21:11 +0200

    Adding docstring for function_definition

--- src/function_proposal/function_defintion.py
+++ src/function_proposal/function_defintion.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def f_oct(w1,w2,w3,x,y) :
+    """f_oct calculates the novelty of a molecule based on it's average tool tanimotos and cosines
+    
+    :param float w1: first weight of novelty function
+    :param float w2: second weight of novelty function
+    :param float w3: third weight of novelty function
+    :param float x: average Tanimoto
+    :param float y: average Cosine
+
+    :return: float value of f (number in [0,1])
+    """
     z1 = np.sin(x)*np.sin(y)
     z2 = np.sin(x+2)*np.sin(y+2)
     z3 = np.sin(x+2)*np.sin(y)
