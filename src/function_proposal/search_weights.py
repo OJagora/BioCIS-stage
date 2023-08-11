06e78a2a0cff695f69463948a96cf67942209441
Octave Jagora <octave.jagora@telecom-paris.fr>
2023-08-11 19:26:10 +0200

    Adding docstring for search_weights

--- src/function_proposal/search_weights.py
+++ src/function_proposal/search_weights.py
@@ -9,6 +9,13 @@ GX,GY,f,index,h_oliv = get_data(path)
 ranked_f = rankdata(f)
 
 def get_weights(n = 100):
+    """get_weights is a function that naively search for the best triplets of weights to maximize the kendall tau's coefficient beetween
+    our novelty predictions and our expert's truth 
+
+    :param int n: resolution of our 3D grid search (number of points beetween -1 et 1 for each weights)
+
+    :return: triplet of weights that maximize the index for our 3D grid of size n x n x n
+    """
     w1 = np.linspace(-1,1,n)
     w2 = np.linspace(-1,1,n)
     w3 = np.linspace(-1,1,n)
@@ -29,3 +36,6 @@ def get_weights(n = 100):
     weights = inds[vals.index(max(vals))]
     print('With w1 : ', weights[0], ' w2 : ', weights[1], ' w3 : ', weights[2])
     return(weights)
+
+    #a gradient descent may be faster, but the number of parameters is quite small, and the precision of our weight values is not 
+    #too important (three digits after zero)
